39893d133cb1727321e41ed07a4ab69faac9e3d9
Mohammad Mashukur Rahman <mr683386@gmail.com>
2025-11-04 20:51:26 +0000

    helath_check.sh script + fixed race condition

--- health_check.sh
+++ health_check.sh
@@ -3,16 +3,30 @@
 # === CONFIG ===
 LOG="/tmp/health.log"
 EMAIL="mashuk.pro@gmail.com"
+MSMTP="/usr/bin/msmtp"
+MSMTPRC="/home/mashuk/.msmtprc"
 THRESHOLD_DISK=90  # % full
 THRESHOLD_MEM=90  # % used
 
 # === FUNCTIONS ===
+timestamp=$(date)
 send_alert() {
-	echo -e "ALERT: $1\n$(date)\n$(hostnamectl hostname)" | msmtp -a  gmail "$EMAIL"
+	local msg="$1"
+	local host=$(/usr/bin/hostnamectl --static)
+    {
+		BODY="ALERT!\n$1\n$host\n$timestamp"
+		echo "From: $EMAIL"
+		echo "To: $EMAIL"
+		echo "Subject: [ALERT] $msg"
+		echo "Content-Type: text/plain; charset=UTF-8"
+		#echo "ALERT: $msg"
+		echo -e "$BODY"
+	} | MSMTP_CONFIG="$MSMTPRC" "$MSMTP" -a gmail "$EMAIL"
+	
 }
 
 # === CHECKS ===
-echo "=== HEALTH CHECK $(date) ===" >> "$LOG"
+echo "=== HEALTH CHECK $timestamp ===" >> "$LOG"
 
 #1.DISK
 DISK=$(df /| tail -1 | awk '{print $5}' | tr -d '%')
@@ -26,4 +40,4 @@ MEM=$(free | awk '/Mem/{printf "%.0f", $3/$2 * 100}')
 LOAD=$(uptime | awk -F 'load average:' '{print $2}' | cut -d, -f1 | xargs)
 [[ $(echo "$LOAD > 2" | bc -l) -eq 1 ]] && send_alert "High load: $LOAD"
 
-echo "Check complete >> "$LOG"
+echo "Check complete" >> "$LOG"
